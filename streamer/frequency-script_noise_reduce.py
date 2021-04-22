3132a26ec08b672eb7745a55108d6b78a02cfa4a
Jamie Hale <jamiehalebc@gmail.com>
2021-04-21 20:37:46 -0700

    Update

--- streamer/frequency-script_noise_reduce.py
+++ streamer/frequency-script_noise_reduce.py
@@ -20,12 +20,13 @@ TIMESTAMP = str(calendar.timegm(time.gmtime()))
 CHUNK = 4410
 RATE = 44100
 CHANNELS = 1
-OUTPUT_WAV_FILE = './streams/stream_' + TIMESTAMP + '.wav'
+OUTPUT_RAW_WAV_FILE = './streams/stream_raw_' + TIMESTAMP + '.wav'
+OUTPUT_REDUCED_WAV_FILE = './streams/stream_reduced_' + TIMESTAMP + '.wav'
 OUTPUT_PEAK_FILE = './peaks/peaks_' + TIMESTAMP + '.csv'
 NOISE_FILE = './noise.wav'
 SLOPE = 10  # Used to control the peak detection sensitivity
 
-global AUDIO
+global RAWAUDIO
 
 total_list = [210.0, 220.0, 230.0, 240.0, 250.0, 260.0, 270.0, 280.0, 290.0, 300.0,
               310.0, 320.0, 330.0, 340.0, 350.0, 360.0, 370.0, 380.0, 390.0, 400.0,
@@ -42,7 +43,8 @@ noise_data = noise_data / 1.0
 
 p = pyaudio.PyAudio()
 
-AUDIO = []
+RAWAUDIO = []
+REDUCEDAUDIO = []
 
 
 def signal_handler(sig, frame):
@@ -114,31 +116,39 @@ def process_second(second):
 
 
 def write_audio_file():
-    global AUDIO
-    print('\nwriting audio file ' + OUTPUT_WAV_FILE)
-    AUDIO = np.array(AUDIO, dtype=np.int16)
+    global RAWAUDIO
+    global REDUCEDAUDIO
+    print('\nwriting audio file ' + OUTPUT_RAW_WAV_FILE)
+    RAWAUDIO = np.array(RAWAUDIO, dtype=np.int16)
+    REDUCEDAUDIO = np.array(REDUCEDAUDIO, dtype=np.int16)
     p.terminate()
 
-    wf = wave.open(OUTPUT_WAV_FILE, 'wb')
+    wf = wave.open(OUTPUT_RAW_WAV_FILE, 'wb')
     wf.setnchannels(CHANNELS)
     wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
     wf.setframerate(RATE)
-    wf.writeframes(b''.join(AUDIO))
+    wf.writeframes(b''.join(RAWAUDIO))
+    wf.close()
+
+    wf = wave.open(OUTPUT_REDUCED_WAV_FILE, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(REDUCEDAUDIO))
     wf.close()
 
     sys.exit(0)
 
 
 def noise_reduce(second):
-    # global AUDIO
+    # global RAWAUDIO
     reduced_second = nr.reduce_noise(
         second.flatten(), noise_data.flatten(), verbose=False)
+    REDUCEDAUDIO.append(reduced_second)
     process_second(reduced_second)
 
 
-stream = p.open(format=pyaudio.paInt16, channels=CHANNELS, rate=RATE, input=True,
-                input_device_index=6,
-                frames_per_buffer=CHUNK)
+stream = p.open(format=pyaudio.paInt16, channels=CHANNELS, rate=RATE, input=True, input_device_index=6, frames_per_buffer=CHUNK)
 
 #
 # Collect 1 sec
@@ -146,8 +156,8 @@ stream = p.open(format=pyaudio.paInt16, channels=CHANNELS, rate=RATE, input=True
 i = 0
 second = np.zeros([10, CHUNK])
 while True:
-    data = np.frombuffer(stream.read(
-        CHUNK, exception_on_overflow=False), dtype=np.int16)
+    data = np.frombuffer(stream.read(CHUNK, exception_on_overflow=False), dtype=np.int16)
+    RAWAUDIO.append(data)
     second[i] = data / 1.0
     i += 1
     if i % 10 == 0:
